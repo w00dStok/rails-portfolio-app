60976c2e82098c8109276bc32291a985853b0d66
w00dStok <woodmatthewj@yahoo.com>
2018-10-11 16:38:16 -0400

    Integrated custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -12,7 +20,8 @@
     minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, 
     nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit 
     qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui 
-    dolorem eum fugiat quo voluptas nulla pariatur?"
+    dolorem eum fugiat quo voluptas nulla pariatur?",
+    topic_id: Topic.last.id
   )
 end
 
@@ -27,10 +36,10 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio Title: #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
     exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
@@ -42,4 +51,18 @@ puts "5 skills created"
   )
 end
 
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio Title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+    anim id est laborum.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
+  )
+end
 puts "9 portfolio items created"
