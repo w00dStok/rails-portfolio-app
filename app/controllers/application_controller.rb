a173856f4d4a887911b630e7514488e19c01141e
w00dStok <woodmatthewj@yahoo.com>
2019-05-20 20:06:48 -0400

    Added copyright helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = WoodSoftwareViewTool::Renderer.copyright "Matthew Wood", "All Rights Reserved"
+  end
+end
+
+module WoodSoftwareViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
